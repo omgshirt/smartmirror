6c9ff644749092530c998cf0bc12c36eaec86f4c
Harout <haroutter@gmail.com>
2015-11-15 15:46:51 -0800

    Facebook login/auth from activity

---
+++ app/src/main/java/org/main/smartmirror/smartmirror/FacebookActivity.java
@@ -0,0 +1,58 @@
+package org.main.smartmirror.smartmirror;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+
+public class FacebookActivity extends Activity{
+
+    LoginButton mLoginButton;
+    CallbackManager mCallBackManager;
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        mCallBackManager = CallbackManager.Factory.create();
+
+        setContentView(R.layout.facebook_fragment);
+        mLoginButton = (LoginButton) findViewById(R.id.fb_login_button);
+        mLoginButton.setReadPermissions("user_friends");
+
+        // Callback registration
+        mLoginButton.registerCallback(mCallBackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // App code
+                Log.i("result: ", "SUCCESS!");
+            }
+
+            @Override
+            public void onCancel() {
+                // App code
+            }
+
+            @Override
+            public void onError(FacebookException exception) {
+                // App code
+            }
+        });
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallBackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+}
