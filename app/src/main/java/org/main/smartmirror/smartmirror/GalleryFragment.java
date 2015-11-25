b8cce69e49fcdd04699cda04c31b8dc72fb510bc
klinster <contact@klinster.com>
2015-11-24 22:38:39 -0800

    Initial Gallery stuff

--- app/src/main/java/org/main/smartmirror/smartmirror/GalleryFragment.java
+++ app/src/main/java/org/main/smartmirror/smartmirror/GalleryFragment.java
@@ -2,29 +2,80 @@ package org.main.smartmirror.smartmirror;
 
 
 import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Fragment that displays a gallery using images located on this application
  */
 public class GalleryFragment extends Fragment {
 
+    private ArrayList<String> mImageList;
+    private Drawable mImageDrawable;
+    private ImageView mGalleryItem;
+    private int mRandomNumber;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        String[] imageGalleryNames = null;
         // get the assets
         AssetManager assetManager = getContext().getAssets();
+        try {
+            imageGalleryNames = assetManager.list("gallery");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        mImageList = new ArrayList<>(Arrays.asList(imageGalleryNames));
+
+        Runnable imageRunnable=new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (true) {
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Random imageRandomizer = new Random();
+                                mRandomNumber = imageRandomizer.nextInt(mImageList.size());
+                            }
+                        });
+                        Thread.sleep(8000L);
+                    }
+                } catch (InterruptedException iex) {}
+            }
+        };
+        Thread imageThread=new Thread(imageRunnable);
+        imageThread.start();
+
+        try {
+            InputStream is = getContext().getAssets().open("gallery/" + mImageList.get(mRandomNumber));
+            mImageDrawable = Drawable.createFromStream(is, null);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
-    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.gallery_fragment, container, false);
+        mGalleryItem = (ImageView) view.findViewById(R.id.gallery_item);
+        mGalleryItem.setImageDrawable(mImageDrawable);
+        return view;
     }
 }
--- app/src/main/java/org/main/smartmirror/smartmirror/QuotesFragment.java
+++ app/src/main/java/org/main/smartmirror/smartmirror/QuotesFragment.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.TextView;
 import java.io.IOException;
 import java.io.InputStream;
@@ -20,6 +22,9 @@ public class QuotesFragment extends Fragment {
     private TextView mquote;
     private TextView mTitle;
     private Typeface mQuoteFont;
+    private Animation mFadeIn;
+    private Animation mFadeOut;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,42 +35,54 @@ public class QuotesFragment extends Fragment {
         mQuoteFont = Typeface.createFromAsset(getContext().getAssets(), "fonts/AlexBrush.ttf");
         InputStream input;
         final String[] parts;
+        byte[] buffer;
+        //setting the fade in an out
+        mFadeIn = new AlphaAnimation(0.0f, 1.0f);
+        mFadeIn.setDuration(3000);
+
+        mFadeOut = new AlphaAnimation(1.0f, 0.0f);
+        mFadeOut.setDuration(3000);
+        String text = null;
         try {
             input = assetManager.open("quotes");
             int size = input.available();
-            byte[] buffer = new byte[size];
+            buffer = new byte[size];
             input.read(buffer);
             input.close();
             // byte buffer into a string
-            String text = new String(buffer);
-            //break the string to parts by lines
-            parts = text.split("\n");
-            //Runnable thread portion
-            Runnable quote_runnable=new Runnable() {
-                @Override
-                public void run() {
-                    try {
-                        while (true) {
-                            getActivity().runOnUiThread(new Runnable() {
-                                @Override
-                                public void run() {
-                                    Random quoteRandomizer = new Random();
-                                    int random_number = quoteRandomizer.nextInt(176);
-                                    mquote.setText(parts[random_number].replaceAll("[0-9]", ""));
-                                }
-                            });
-                            Thread.sleep(8000L);
-                        }
-                    } catch (InterruptedException iex) {}
-                }
-            };
-            Thread quote_thread=new Thread(quote_runnable);
-            quote_thread.start();
+            text = new String(buffer);
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
 
+
+        //break the string to parts by lines
+        parts = text.split("\n");
+        //Runnable thread portion
+        Runnable quoteRunnable = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (true) {
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Random quoteRandomizer = new Random();
+                                int random_number = quoteRandomizer.nextInt(176);
+                                mquote.setText(parts[random_number]);
+                                mquote.startAnimation(mFadeIn);
+                            }
+                        });
+                        Thread.sleep(8000L);
+                    }
+                } catch (InterruptedException iex) {
+
+                }
+            }
+        };
+        Thread quoteThread = new Thread(quoteRunnable);
+        quoteThread.start();
     }
 
     @Override
@@ -79,4 +96,5 @@ public class QuotesFragment extends Fragment {
         mTitle.setTypeface(mQuoteFont);
         return view;
     }
+
 }
