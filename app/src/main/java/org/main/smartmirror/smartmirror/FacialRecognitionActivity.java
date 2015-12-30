1c4e6a38d4423a48d846afd0768efdb8e4171bae
Navin Row <navin.row.43@my.csun.edu>
2015-12-29 17:31:21 -0800

    Added OpenCV library

---
+++ app/src/main/java/org/main/smartmirror/smartmirror/FacialRecognitionActivity.java
@@ -0,0 +1,11 @@
+package org.main.smartmirror.smartmirror;
+
+/**
+ * Created by Master N on 12/19/2015.
+ */
+
+import org.opencv.android.Utils;
+
+public class FacialRecognitionActivity {
+    //The Beginning
+}
