e80bb0c6a76951228cfd8dc015bd5ab6aed7c0cd
klinster <contact@klinster.com>
2016-02-01 22:54:58 -0800

    Reformat Traffic and Help - Traffic returns JSON!
    -TODO
    Fix help some more
    Get Traffic to display meaningful information

--- app/src/main/java/org/main/smartmirror/smartmirror/HelpFragment.java
+++ app/src/main/java/org/main/smartmirror/smartmirror/HelpFragment.java
@@ -5,16 +5,20 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 /**
  * Class that handles the help dialog which shows the commands the user can issue
  */
 public class HelpFragment extends DialogFragment {
     private AlertDialog.Builder mAlertDialogBuilder;
-    private static final String TITLE="Available Commands";
+    private static final String DIALOG_TITLE="Help";
 
     //used as the constructor to get the new instance of Helper dialogue
     public static HelpFragment newInstance(String name) {
@@ -33,44 +37,50 @@ public class HelpFragment extends DialogFragment {
         Resources res = getResources();
 
         mAlertDialogBuilder = new AlertDialog.Builder(getActivity(),R.style.MyDialog);
-        mAlertDialogBuilder.setTitle(TITLE);
+        mAlertDialogBuilder.setTitle(name + " - " + DIALOG_TITLE);
         mAlertDialogBuilder.setView(vewHelp);
-        TextView txtHelpHeader = (TextView)vewHelp.findViewById(R.id.help_header);
-        TextView txtHelpContent = (TextView)vewHelp.findViewById(R.id.help_content);
+        TextView txtCurrentHelpHeader = (TextView)vewHelp.findViewById(R.id.fragment_help_header);
+        TextView txtCurrentHelpContent = (TextView)vewHelp.findViewById(R.id.fragment_help_content);
+        TextView txtHelpHeader = (TextView)vewHelp.findViewById(R.id.general_help_header);
+        TextView txtHelpContent = (TextView)vewHelp.findViewById(R.id.general_help_content);
+        TextView txtWeahterHelpHeader = (TextView)vewHelp.findViewById(R.id.weather_header);
+        TextView txtWeahterHelpContent = (TextView)vewHelp.findViewById(R.id.weather_content);
 
+        // set the default help!
         String strContent;
+        String[] arrayContent;
+        // general
+        arrayContent = res.getStringArray(R.array.general_help);
+        strContent = stringSpace(arrayContent);
+        txtHelpHeader.setText("Choose a View");
+        txtHelpContent.setText(strContent);
+        // weather
+        arrayContent = res.getStringArray(R.array.weather_help);
+        strContent = stringSpace(arrayContent);
+        txtWeahterHelpHeader.setText("Time & Weather");
+        txtWeahterHelpContent.setText(strContent);
 
         switch (name) {
-
             // night light
-            case Constants.NIGHT_LIGHT:
-                String[] arrayContent = res.getStringArray(R.array.color_names);
+            case Constants.LIGHT:
+                arrayContent = res.getStringArray(R.array.color_names);
                 strContent = stringSpace(arrayContent);
-                txtHelpHeader.setText("Color Options:");
-                txtHelpContent.setText(strContent);
+                txtCurrentHelpHeader.setText("Color Options:");
+                txtCurrentHelpContent.setText(strContent);
                 break;
             // news
             case Constants.NEWS:
                 arrayContent = res.getStringArray(R.array.guardian_sections);
                 strContent = stringSpace(arrayContent);
-                txtHelpHeader.setText("Choose a news section:");
-                txtHelpContent.setText(strContent);
+                txtCurrentHelpHeader.setText("Choose a news section:");
+                txtCurrentHelpContent.setText(strContent);
                 break;
-
             // settings
             case Constants.SETTINGS:
                 arrayContent = res.getStringArray(R.array.help_settings2);
                 strContent = stringSpace(arrayContent);
-                txtHelpHeader.setText("General Settings:");
-                txtHelpContent.setText(strContent);
-                break;
-
-            // default help
-            default:
-                arrayContent = res.getStringArray(R.array.general_help);
-                strContent = stringSpace(arrayContent);
-                txtHelpHeader.setText("Choose a View");
-                txtHelpContent.setText(strContent);
+                txtCurrentHelpHeader.setText("General Settings:");
+                txtCurrentHelpContent.setText(strContent);
                 break;
         }
         return mAlertDialogBuilder.create();
@@ -79,6 +89,8 @@ public class HelpFragment extends DialogFragment {
     public String stringSpace(String[] string){
         String str = "";
         for(int i = 0; i < string.length; i++){
+            // capitalize the first letter in the arrays
+            string[i] = string[i].substring(0,1).toUpperCase() + string[i].substring(1);
             str += string[i] + "\n";
         }
         return str;
--- app/src/main/java/org/main/smartmirror/smartmirror/TrafficFragment.java
+++ app/src/main/java/org/main/smartmirror/smartmirror/TrafficFragment.java
@@ -1,57 +1,102 @@
 package org.main.smartmirror.smartmirror;
 
-import android.location.Location;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Fragment that handles the traffic information
  */
 public class TrafficFragment extends Fragment {
-    private float mZoom;
-    private GoogleMap mGoogleMap;
-    private MapView mMapView;
     private Preferences mPreferance;
+    private String mCurrentLat;
+    private String mCurrentLong;
+    private String mWorkLat;
+    private String mWorkLong;
+    private TextView txtDetails;
+    private TextView txtDelays;
 
+
+    Handler mHandler = new Handler();
+// https://maps.googleapis.com/maps/api/distancematrix/json?origins=34.0636439,-118.2593811&destinations=34.2370851,-118.5272547&departure_time=now&traffic_model=best_guess&key=AIzaSyBumZObXEyI5_7Ie0u8ZnrRKAXzojKpDw8
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mPreferance = Preferences.getInstance(getActivity());
-        mZoom = 14.0f;
+        mCurrentLat = Double.toString(mPreferance.getLatitude());
+        mCurrentLong = Double.toString(mPreferance.getLongitude());
+        //csun
+        mWorkLat = "34.2370851";
+        mWorkLong = "-118.5272547";
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.traffic_fragment, container, false);
-        mMapView = (MapView) view.findViewById(R.id.map);
-        mMapView.onCreate(savedInstanceState);
-        mMapView.onResume();// needed to get the map to display immediately
-        mGoogleMap = mMapView.getMap();
-        setUpMap();
+        txtDetails = (TextView) view.findViewById(R.id.traffic_details);
+        txtDelays = (TextView) view.findViewById(R.id.traffic_delay);
         return view;
     }
 
-    private void setUpMap(){
+    @Override
+    public void onStart() {
+        super.onStart();
+        startTrafficUpdate();
+    }
+
+    private void startTrafficUpdate(){
+        String distanceMatrixRequest = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=%s,%s&destinations=%s,%s&departure_time=now&traffic_model=best_guess&units=%s&key=%s";
+        String distanceMatrixKey = "AIzaSyBumZObXEyI5_7Ie0u8ZnrRKAXzojKpDw8";
+        String distanceMatrixUnit = "metric";
+        if(mPreferance.getWeatherUnits().equals(Preferences.ENGLISH)){
+            distanceMatrixUnit = "imperial";
+        }
+        updateTrafficData(String.format(distanceMatrixRequest, mCurrentLat, mCurrentLong, mWorkLat, mWorkLong, distanceMatrixUnit, distanceMatrixKey));
+
+    }
+
+    private void renderTraffic(JSONObject json){
         try {
-            MapsInitializer.initialize(getActivity().getApplicationContext());
-        } catch (Exception e) {
+            JSONArray data = json.getJSONArray("rows");
+            Log.d("traffic", data.toString());
+
+        } catch (JSONException e) {
             e.printStackTrace();
         }
-        LatLng currentPosition = new LatLng(mPreferance.getLatitude(),mPreferance.getLongitude());
-        LatLng workPosition = new LatLng(34.2415936,-118.5286617);
-        mGoogleMap.addMarker(new MarkerOptions().position(currentPosition).title("You're here!"));
-        mGoogleMap.addMarker(new MarkerOptions().position(workPosition).title("Work"));
-        mGoogleMap.setTrafficEnabled(true);
-        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentPosition, mZoom));
+    }
+
+    private void updateTrafficData(final String request){
+        new Thread(){
+            public void run(){
+                final JSONObject json = FetchURL.getJSON(request);
+                if(json == null){
+                    mHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getActivity(),
+                                    "error no traffic found",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    });
+                } else {
+                    mHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            renderTraffic(json);
+                        }
+                    });
+                }
+            }
+        }.start();
     }
 }
