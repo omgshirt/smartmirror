9e55e181ec9aa1fe2b5f85cba9dc2fd27fa8eab4
klinster <contact@klinster.com>
2016-02-17 22:49:54 -0800

    Populate the Google Account picker. A few UI changes to AccountActivity
    TODO
    - Save the Google Account with callback
    - Add listeners to the Edit Text widgets.

--- app/src/main/java/org/main/smartmirror/smartmirror/AccountActivity.java
+++ app/src/main/java/org/main/smartmirror/smartmirror/AccountActivity.java
@@ -1,17 +1,23 @@
 package org.main.smartmirror.smartmirror;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 /**
  * Activity that handles the Account Credentials and Work address
  */
-public class AccountActivity extends AppCompatActivity {
+public class AccountActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private Preferences mPreferences;
 
@@ -20,9 +26,39 @@ public class AccountActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.account_activity);
         mPreferences = Preferences.getInstance(this);
+        getAccounts();
     }
 
-    public void startMain(View view){
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+
+    private void getAccounts() {
+        Spinner googleAccountsPicker = (Spinner) findViewById(R.id.google_account_picker);
+
+        ArrayList<String> accountsList = new ArrayList<>();
+        //Getting all registered Google Accounts on device
+        try {
+            Account[] accounts = AccountManager.get(this).getAccountsByType("com.google");
+            for (Account account : accounts) {
+//                AccountItem item = new AccountItem(account.name);
+                accountsList.add(account.name);
+            }
+        } catch (Exception e) {
+            Log.i("Exception", "Exception:" + e);
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, accountsList);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        googleAccountsPicker.setAdapter(adapter);
+    }
+
+    public void startMain(View view) {
         Intent intent = new Intent(this, MainActivity.class);
         EditText workAddress = (EditText) findViewById(R.id.work_location);
         String strAddress = workAddress.getText().toString().replace(' ', '+');
