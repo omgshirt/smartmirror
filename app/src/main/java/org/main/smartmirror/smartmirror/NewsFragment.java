7dfb4f207d5c60c4b016b20f746dd548d0d11e63
harout <haroutter@gmail.com>
2016-03-07 17:14:39 -0800

    fixed casting issues for news article numbers

--- app/src/main/java/org/main/smartmirror/smartmirror/NewsFragment.java
+++ app/src/main/java/org/main/smartmirror/smartmirror/NewsFragment.java
@@ -315,9 +315,24 @@ public class NewsFragment extends Fragment implements CacheManager.CacheListener
     }
 
     public void addToArrayList(ArrayList arrayList, int numItems) {
-        for (int i = 0; i < numItems; i++) {
-            arrayList.add(i, i);
-        }
+        /*for (int i = 0; i < numItems; i++) {
+            arrayList.add(i);
+        }*/
+        arrayList.add("1");
+        arrayList.add("2");
+        arrayList.add("3");
+        arrayList.add("4");
+        arrayList.add("5");
+        arrayList.add("6");
+        arrayList.add("7");
+        arrayList.add("8");
+        arrayList.add("9");
+        arrayList.add("10");
+        arrayList.add("11");
+        arrayList.add("12");
+        arrayList.add("13");
+        arrayList.add("14");
+        arrayList.add("15");
     }
 
     /**
