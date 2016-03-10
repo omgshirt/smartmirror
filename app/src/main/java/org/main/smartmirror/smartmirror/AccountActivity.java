1ae7a8a6234373af6d502bd9212f1ea371de5037
klinster <contact@klinster.com>
2016-03-09 23:35:08 -0800

    Move Permissions to AccountActivity
    TODO
    - Need to set the flag to "Never Ask"

--- app/src/main/java/org/main/smartmirror/smartmirror/AccountActivity.java
+++ app/src/main/java/org/main/smartmirror/smartmirror/AccountActivity.java
@@ -1,11 +1,20 @@
 package org.main.smartmirror.smartmirror;
 
+import android.Manifest;
 import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -25,15 +34,14 @@ import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
-import javax.crypto.SecretKey;
+import java.util.Map;
 
 import io.fabric.sdk.android.Fabric;
 
 /**
  * Activity that handles the Account Credentials and Work address
- * TODO handle all the permissions for MainActivity Here.
  */
 public class AccountActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
@@ -46,15 +54,19 @@ public class AccountActivity extends AppCompatActivity implements AdapterView.On
     public String mAuthToken;
     public String mAuthSecret;
 
+    public static final int REQUEST_PERMISSIONS = 0;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWriteSettingsPermission();
         TwitterAuthConfig authConfig = new TwitterAuthConfig(Constants.TWITTER_CONSUMER_KEY, Constants.TWITTER_CONSUMER_SECRET);
         Fabric.with(this, new TwitterCore(authConfig));
         setContentView(R.layout.account_activity);
         mPreference = Preferences.getInstance(this);
-        findGoogleAccounts();
         setUpTwitterButton();
+        askForPermissions();
         if (mPreference.getFirstTimeRun()) {
             // generate the keys
             // createNewKeys();
@@ -91,9 +103,60 @@ public class AccountActivity extends AppCompatActivity implements AdapterView.On
         mTwitterLoginButton.onActivityResult(requestCode, resultCode, data);
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PERMISSIONS:
+                Map<String, Integer> perms = new HashMap<String, Integer>();
+                // Initial
+                perms.put(Manifest.permission.ACCESS_COARSE_LOCATION, PackageManager.PERMISSION_GRANTED);
+                perms.put(Manifest.permission.CAMERA, PackageManager.PERMISSION_GRANTED);
+                perms.put(Manifest.permission.GET_ACCOUNTS, PackageManager.PERMISSION_GRANTED);
+                perms.put(Manifest.permission.READ_CALENDAR, PackageManager.PERMISSION_GRANTED);
+                perms.put(Manifest.permission.RECORD_AUDIO, PackageManager.PERMISSION_GRANTED);
+                perms.put(Manifest.permission.WRITE_EXTERNAL_STORAGE, PackageManager.PERMISSION_GRANTED);
+                // Fill with results
+                for (int i = 0; i < permissions.length; i++)
+                    perms.put(permissions[i], grantResults[i]);
+                // Check for permissions
+                if (perms.get(Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                        && perms.get(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+                        && perms.get(Manifest.permission.GET_ACCOUNTS) == PackageManager.PERMISSION_GRANTED
+                        && perms.get(Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED
+                        && perms.get(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                    // All Permissions Granted
+                    findGoogleAccounts();
+                } else {
+                    // Permission Denied
+                    Toast.makeText(AccountActivity.this, "Some Permission is Denied", Toast.LENGTH_LONG).show();
+                }
+
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    private void getWriteSettingsPermission() {
+        // check for permission to write system settings on API 23 and greater.
+        // Leaving this in case we need the WRITE_SETTINGS permission later on.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.System.canWrite(getApplicationContext())) {
+                Intent intent = new Intent(android.provider.Settings.ACTION_MANAGE_WRITE_SETTINGS);
+                intent.setData(Uri.parse("package:" + this.getPackageName()));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+//                Intent intent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+//                startActivityForResult(intent, REQUEST_PERMISSIONS);
+            }
+        }
+    }
+
     /**
      * Sets up the functionality for the twitter button.
      */
+
     private void setUpTwitterButton() {
         mTwitterLoginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
         mTwitterLoginButton.setCallback(new Callback<TwitterSession>() {
@@ -148,6 +211,62 @@ public class AccountActivity extends AppCompatActivity implements AdapterView.On
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         googleAccountsPicker.setAdapter(adapter);
         googleAccountsPicker.setOnItemSelectedListener(this);
+    }
+
+    /**
+     * Checks all the permissions!
+     */
+    private void askForPermissions() {
+        List<String> permissionsNeeded = new ArrayList<>();
+        final List<String> permissionsList = new ArrayList<>();
+        // Here, this is the current activity
+        if (!addPermission(permissionsList, Manifest.permission.ACCESS_COARSE_LOCATION))
+            permissionsNeeded.add("Access Coarse Location");
+        if (!addPermission(permissionsList, Manifest.permission.CAMERA))
+            permissionsNeeded.add("Camera");
+        if (!addPermission(permissionsList, Manifest.permission.GET_ACCOUNTS))
+            permissionsNeeded.add("Get Accounts");
+        if (!addPermission(permissionsList, Manifest.permission.READ_CALENDAR))
+            permissionsNeeded.add("Read Calendar");
+        if (!addPermission(permissionsList, Manifest.permission.RECORD_AUDIO))
+            permissionsNeeded.add("Record Audio");
+        if (!addPermission(permissionsList, Manifest.permission.WRITE_EXTERNAL_STORAGE))
+            permissionsNeeded.add("Write External Storage");
+        if (permissionsList.size() > 0) {
+            if (permissionsNeeded.size() > 0) {
+                // Need Rationale
+                String message = "You need to grant access to " + permissionsNeeded.get(0);
+                for (int i = 1; i < permissionsNeeded.size(); i++)
+                    message = message + ", " + permissionsNeeded.get(i);
+                showMessageOKCancel(message,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                ActivityCompat.requestPermissions(AccountActivity.this, permissionsList.toArray(new String[permissionsList.size()]),
+                                        REQUEST_PERMISSIONS);
+                            }
+                        });
+            }
+        }
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(AccountActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
+
+    private boolean addPermission(List<String> permissionsList, String permission) {
+        if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+            permissionsList.add(permission);
+            // Check for Rationale Option
+            if (!ActivityCompat.shouldShowRequestPermissionRationale(this, permission))
+                return false;
+        }
+        return true;
     }
 
     /**
