9a98a72c84900815428304ed7a63442ab8ac82b2
harout <haroutter@gmail.com>
2016-03-14 21:08:08 -0700

    Able to make picasa api calls, parse user id from xml

--- app/src/main/java/org/main/smartmirror/smartmirror/PhotosFragment.java
+++ app/src/main/java/org/main/smartmirror/smartmirror/PhotosFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.util.Xml;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,6 +18,7 @@ import org.w3c.dom.NodeList;
 
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
 
 import com.google.gdata.client.photos.*;
 
@@ -30,13 +32,16 @@ public class PhotosFragment extends Fragment {
     URL albumPostUrl;
     private PicasawebService service;
     Handler mHandler;
-    String userID;
+    String userID = "smartmirrortesting";
     String albumID;
-    int numPhotos;
+    int numPhotos = 1;
+
+    ArrayList<String> names = new ArrayList<>();
 
     String getAlbums = "https://picasaweb.google.com/data/feed/api/user/" + userID;
     String getPhotosInAlbum = "https://picasaweb.google.com/data/feed/api/user/"+userID+"albumid/"+albumID;
     String getLatestPhotos = "https://picasaweb.google.com/data/feed/api/user/"+userID+"?kind=photo&max-results="+numPhotos;
+    String getUserPhotos = "https://picasaweb.google.com/data/feed/api/user/"+userID;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -44,43 +49,13 @@ public class PhotosFragment extends Fragment {
         View view = inflater.inflate(R.layout.photos_fragment, container, false);
         mPhotoFromPicasa = (ImageView) view.findViewById(R.id.photo_from_picasa);
 
-        //new PhotosASyncTask(service,"smartmirrortesting@gmail.com", "smartmirrort").execute();
-
-
-
+        getXmlFromUrl(getUserPhotos);
 
         return view;
     }
 
     public void renderPhoto(JSONObject json) {
 
-    }
-
-    // Get picasa json
-    private void updatePicasa(final String query) {
-        new Thread() {
-            public void run() {
-                final JSONObject json = FetchURL.getJSON(query);
-                if (json == null) {
-                    mHandler.post(new Runnable() {
-                        public void run() {
-                            ((MainActivity) getActivity()).showToast(getString(R.string.photos_err),
-                                    Gravity.CENTER, Toast.LENGTH_LONG);
-                        }
-                    });
-                } else {
-                    mHandler.post(new Runnable() {
-                        public void run() {
-                            try {;
-                                Log.i("PICASA ", json.toString());
-                                //renderNews(json);
-                            } catch (Exception e) {Log.i("picasa ", e.toString());}
-
-                        }
-                    });
-                }
-            }
-        }.start();
     }
 
 
@@ -95,13 +70,14 @@ public class PhotosFragment extends Fragment {
                     DocumentBuilder builder = factory.newDocumentBuilder();
                     Document doc = builder.parse(conn.getInputStream());
 
-                    /*NodeList nodes = doc.getElementsByTagName(*//*tag from xml file*//*);
+                    NodeList nodes = doc.getElementsByTagName("author");
                     for (int i = 0; i < nodes.getLength(); i++) {
                         Element element = (Element) nodes.item(i);
-                        NodeList title = element.getElementsByTagName(*//*item within the tag*//*);
+                        NodeList title = element.getElementsByTagName("name");
                         Element line = (Element) title.item(0);
-                        phoneNumberList.add(line.getTextContent());
-                    }*/
+                        names.add(line.getTextContent());
+                        Log.i("Node ", names.toString());
+                    }
                 }
                 catch (Exception e) {
                     e.printStackTrace();
