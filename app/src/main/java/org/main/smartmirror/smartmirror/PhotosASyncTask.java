483366e23c45f4cde6796fbdc3f10a40d24d6b68
harout <haroutter@gmail.com>
2016-03-16 20:14:24 -0700

    load sample image from album

--- app/src/main/java/org/main/smartmirror/smartmirror/PhotosASyncTask.java
+++ app/src/main/java/org/main/smartmirror/smartmirror/PhotosASyncTask.java
@@ -4,19 +4,89 @@ import android.os.AsyncTask;
 
 import com.squareup.picasso.Picasso;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import java.net.URL;
+import java.net.URLConnection;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+
 
 public class PhotosASyncTask extends AsyncTask<String, Void, String> {
-
+    String userID = "smartmirrortesting";
+    String albumID = "SmartMirror";
+    String getUserPhotos = "https://picasaweb.google.com/data/feed/api/user/"+userID;
+    String imageUrl;
+    String samplePhoto = "https://lh3.googleusercontent.com/-c7yXykzq6uw/VuXZevOmjhE/AAAAAAAAALw/HdMLGlY50d8//SmartMirror";
 
     protected String doInBackground(String[] params) {
         try {
-            //Picasso.with(MainActivity.getContextForApplication()).load(PhotosFragment.mImageUrlList.get(0)).fit().centerInside().into(PhotosFragment.mPhotoFromPicasa);
+            getXmlFromUrl(getUserPhotos);
         } catch (Exception e) {e.printStackTrace();}
+
         return "success";
     }
 
     protected void onPostExecute(String message) {
+        try {
+            Picasso.with(MainActivity.getContextForApplication()).load(imageUrl).fit().centerInside().into(PhotosFragment.mPhotoFromPicasa);
+        } catch (Exception e) {e.printStackTrace();}
+    }
 
+    private void getXmlFromUrl(final String query) {
+        new Thread() {
+            public void run() {
+                try {
+                    URL url = new URL(query);
+                    URLConnection conn = url.openConnection();
+
+                    // get xml from api
+                    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                    DocumentBuilder builder = factory.newDocumentBuilder();
+                    Document doc = builder.parse(conn.getInputStream());
+                    traverse(doc.getDocumentElement());
+
+
+
+                    /*NodeList nodes = doc.getElementsByTagName("author");
+                    //Log.i("Node ", nodes.toString());
+                    for (int i = 0; i < nodes.getLength(); i++) {
+                        Element element = (Element) nodes.item(i);
+                        NodeList title = element.getElementsByTagName("name");
+                        Element line = (Element) title.item(0);
+                        //names.add(line.getTextContent());
+                        //Log.i("Node ", names.toString());
+                    }*/
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
+
+    }
+
+    public void traverse(Node node) {
+        NodeList list = node.getChildNodes();
+
+        for (int i = 0; i < list.getLength(); i++) {
+            Node currentNode = list.item(i);
+            traverse(currentNode);
+
+        }
+
+        if (node.getNodeName().equals("media:content")) {
+            Element durationElement = (Element) node;
+            System.out.println(durationElement.getAttribute("url"));
+            imageUrl = durationElement.getAttribute("url");
+
+            //new PhotosASyncTask().execute();
+        }
     }
 
 
--- app/src/main/java/org/main/smartmirror/smartmirror/PhotosFragment.java
+++ app/src/main/java/org/main/smartmirror/smartmirror/PhotosFragment.java
@@ -40,7 +40,7 @@ import javax.xml.transform.stream.StreamResult;
 
 public class PhotosFragment extends Fragment {
 
-    ImageView mPhotoFromPicasa;
+    public static ImageView mPhotoFromPicasa;
     URL albumPostUrl;
     Handler mHandler;
     String userID = "smartmirrortesting";
@@ -48,7 +48,9 @@ public class PhotosFragment extends Fragment {
     int numPhotos = 1;
     ArrayList<Uri> mImageUrlList = new ArrayList<Uri>();
     String[] imageList = new String[10];
-    ListView photosFeed;
+    String imageUrl;
+
+    String samplePhoto = "https://lh3.googleusercontent.com/-c7yXykzq6uw/VuXZevOmjhE/AAAAAAAAALw/HdMLGlY50d8//SmartMirror";
 
     String getAlbums = "https://picasaweb.google.com/data/feed/api/user/" + userID;
     String getPhotosInAlbum = "https://picasaweb.google.com/data/feed/api/user/"+userID+"/albumid/"+albumID;
@@ -59,98 +61,19 @@ public class PhotosFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.photos_fragment, container, false);
-        //mPhotoFromPicasa = (ImageView) view.findViewById(R.id.photo_from_picasa);
-        photosFeed = (ListView) view.findViewById(R.id.list_photos);
+        mPhotoFromPicasa = (ImageView) view.findViewById(R.id.photo_from_picasa);
 
-
-        getXmlFromUrl(getUserPhotos);
-        //renderPhoto();
+        renderPhoto();
 
         return view;
     }
 
     public void renderPhoto() {
-        //Picasso.with(getActivity()).load(mImageUrlList.get(0)).fit().centerInside().into(mPhotoFromPicasa);
+        Picasso.with(getActivity()).load(samplePhoto).fit().centerInside().into(mPhotoFromPicasa);
         //new PhotosASyncTask().execute();
-
-        ArrayList<CustomListViewObject> objects = new ArrayList<CustomListViewObject>();
-        CustomAdapter customAdapter = new CustomAdapter(getActivity(), objects);
-        try {
-            for(int j = 0; j < 10; j++){
-                CustomListViewObject co = new CustomListViewObject(null,null,mImageUrlList.get(j), null);
-                objects.add(co);
-                customAdapter.notifyDataSetChanged();
-            }
-
-        } catch (Exception e) {Log.i("PHOTOS", e.toString());}
-        photosFeed.setAdapter(customAdapter);
     }
 
 
-    private void getXmlFromUrl(final String query) {
-        new Thread() {
-            public void run() {
-                try {
-                    URL url = new URL(query);
-                    URLConnection conn = url.openConnection();
-
-                    // get xml from api
-                    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-                    DocumentBuilder builder = factory.newDocumentBuilder();
-                    Document doc = builder.parse(conn.getInputStream());
-                    traverse(doc.getDocumentElement());
-
-
-
-                    /*NodeList nodes = doc.getElementsByTagName("author");
-                    //Log.i("Node ", nodes.toString());
-                    for (int i = 0; i < nodes.getLength(); i++) {
-                        Element element = (Element) nodes.item(i);
-                        NodeList title = element.getElementsByTagName("name");
-                        Element line = (Element) title.item(0);
-                        //names.add(line.getTextContent());
-                        //Log.i("Node ", names.toString());
-                    }*/
-                }
-                catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }.start();
-
-    }
-
-    public void traverse(Node node) {
-        NodeList list = node.getChildNodes();
-        String imageUrl;
-        for (int i = 0; i < list.getLength(); i++) {
-            Node currentNode = list.item(i);
-            traverse(currentNode);
-
-        }
-
-        for (int j = 0; j < list.getLength(); j++) {
-            if (node.getNodeName().equals("media:content")) {
-                Element durationElement = (Element) node;
-                System.out.println(durationElement.getAttribute("url"));
-                imageUrl = durationElement.getAttribute("url");
-                mImageUrlList.add(j, Uri.parse(imageUrl));
-                Log.i("PHOTO URL", mImageUrlList.get(j).toString());
-                imageList[j] = imageUrl;
-            }
-        }
-        /*try {
-            //Picasso.with(getContext()).load(imageList[0]).fit().centerInside().into(mPhotoFromPicasa);
-            Handler uiHandler = new Handler(Looper.getMainLooper());
-            uiHandler.post(new Runnable(){
-                @Override
-                public void run() {
-                    Picasso.with(getContext()).load(mImageUrlList.get(0)).fit().centerInside().into(mPhotoFromPicasa);
-                }
-            });
-        } catch (Exception e) {e.printStackTrace();}*/
-
-    }
 
 
 }
