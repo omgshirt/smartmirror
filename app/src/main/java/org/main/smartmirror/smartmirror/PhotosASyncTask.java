d715c8352dbc8765a73d55c11b7b653ec06e2986
harout <haroutter@gmail.com>
2016-03-17 21:59:42 -0700

    get all photos from an album via albumid (hardcoded)

--- app/src/main/java/org/main/smartmirror/smartmirror/PhotosASyncTask.java
+++ app/src/main/java/org/main/smartmirror/smartmirror/PhotosASyncTask.java
@@ -2,6 +2,7 @@ package org.main.smartmirror.smartmirror;
 
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Handler;
 import android.util.Log;
 
 import com.squareup.picasso.Picasso;
@@ -11,25 +12,43 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+import java.io.StringWriter;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.RunnableFuture;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 public class PhotosASyncTask extends AsyncTask<String, Void, String> {
 
-    String userID = "smartmirrortesting";
-    String albumID = "SmartMirror";
-    int numPhotos = 1;
-    ArrayList<Uri> mImageUrlList = new ArrayList<Uri>();
+    String userID = "smartmirrortesting"; // user ID
+    String uID = "118328364730024898386"; // user ID??
+    String albumID = "6261649979025559057"; // smart mirror album
+    //String albumID = "6263091469173478818"; // profile pics
+    int numPhotos = 4;
+    public static ArrayList<Uri> mImageUrlList = new ArrayList<Uri>();
     String imageUrl;
+    //public NodeList nodeList;
 
-    String getAlbums = "https://picasaweb.google.com/data/feed/api/user/" + userID;
+
+    Handler mTimerHandler = new Handler();
+    int mDelay = 5000; //milliseconds
+
+    String getAlbums = "https://picasaweb.google.com/data/feed/api/user/" + uID;
     String getPhotosInAlbum = "https://picasaweb.google.com/data/feed/api/user/"+userID+"/albumid/"+albumID;
-    String getLatestPhotos = "https://picasaweb.google.com/data/feed/api/user/"+userID+"?kind=photo&max-results="+numPhotos;
-    String getUserPhotos = "https://picasaweb.google.com/data/feed/api/user/"+userID;
+    String getLatestPhotos = "https://picasaweb.google.com/data/feed/api/user/"+uID+"?kind=photo&max-results="+numPhotos;
+    String getUserPhotos = "https://picasaweb.google.com/data/feed/api/user/"+uID;
+    //String getAlbums = "https://picasaweb.google.com/data/feed/api/user/"+userID+"?kind=album";
 
 
 
@@ -37,24 +56,48 @@ public class PhotosASyncTask extends AsyncTask<String, Void, String> {
     protected String doInBackground(String[] params) {
 
         try {
-            getXmlFromUrl(getUserPhotos);
+            getXmlFromUrl(getPhotosInAlbum);
 
         }catch (Exception e) {
             Log.i("ERR ", e.toString());
 
         }
-
         return "SUCCESS";
-
     }
 
     @Override
     protected void onPostExecute(String message) {
         try {
-            Picasso.with(MainActivity.getContextForApplication()).load(mImageUrlList.get(0)).fit().centerInside().into(PhotosFragment.mPhotoFromPicasa);
+                /*Timer timer = new Timer();
+                TimerTask myTask = new TimerTask() {
+                    @Override
+                    public void run() {
+                        Picasso.with(MainActivity.getContextForApplication()).load(mImageUrlList.get(currentPhoto)).fit().centerInside().into(PhotosFragment.mPhotoFromPicasa);
+                        //currentPhoto++;
+                        //if (currentPhoto > 2) currentPhoto = 0;
+
+                    }
+                };
+                timer.schedule(myTask, 1000, 3000);*/
+
+
         } catch (Exception e) {e.printStackTrace();}
 
     }
+
+    private String nodeToString(Node node) {
+        StringWriter sw = new StringWriter();
+        try {
+            Transformer t = TransformerFactory.newInstance().newTransformer();
+            t.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+            t.transform(new DOMSource(node), new StreamResult(sw));
+        } catch (TransformerException te) {
+            te.printStackTrace();
+        }
+        return sw.toString();
+    }
+
+
 
     private void getXmlFromUrl(final String query) {
         try {
@@ -64,6 +107,8 @@ public class PhotosASyncTask extends AsyncTask<String, Void, String> {
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
             DocumentBuilder builder = factory.newDocumentBuilder();
             Document doc = builder.parse(conn.getInputStream());
+            String xmlString = nodeToString(doc.getDocumentElement());
+            Log.i("FULL XML", xmlString);
             traverse(doc.getDocumentElement());
         }
         catch (Exception e) {
@@ -72,23 +117,23 @@ public class PhotosASyncTask extends AsyncTask<String, Void, String> {
     }
 
     public void traverse(Node node) {
-        NodeList list = node.getChildNodes();
+        NodeList nodeList = node.getChildNodes();
 
-        for (int i = 0; i < list.getLength(); i++) {
-            Node currentNode = list.item(i);
+
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            Node currentNode = nodeList.item(i);
             traverse(currentNode);
         }
 
 
         if (node.getNodeName().equals("media:content")) {
             Element durationElement = (Element) node;
-            System.out.println(durationElement.getAttribute("url"));
+            //System.out.println(durationElement.getAttribute("url"));
             imageUrl = durationElement.getAttribute("url");
             Log.i("PHOTO URL", imageUrl);
             mImageUrlList.add(Uri.parse(imageUrl));
+            //Log.i("PHOTO", mImageUrlList.toString());
         }
-
-
     }
 
 }
--- app/src/main/java/org/main/smartmirror/smartmirror/PhotosFragment.java
+++ app/src/main/java/org/main/smartmirror/smartmirror/PhotosFragment.java
@@ -1,6 +1,7 @@
 package org.main.smartmirror.smartmirror;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,15 +12,36 @@ import android.widget.ImageView;
 public class PhotosFragment extends Fragment {
 
     public static ImageView mPhotoFromPicasa;
+    int currentPhoto = 0;
+    Handler mTimerHandler = new Handler();
+    int mDelay = 5000; //milliseconds
+    PhotosASyncTask newASync = new PhotosASyncTask();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.photos_fragment, container, false);
         mPhotoFromPicasa = (ImageView) view.findViewById(R.id.photo_from_picasa);
+
         new PhotosASyncTask().execute();
+        //updatePhoto();
         return view;
     }
 
+    public void updatePhoto() {
+        while (currentPhoto < 3) {
+            mTimerHandler.postDelayed(new Runnable(){
+                public void run(){
+                    System.out.println("CYCLING NEXT PHOTO");
+                    //Picasso.with(MainActivity.getContextForApplication()).load(PhotosASyncTask.mImageUrlList.get(currentPhoto)).fit().centerInside().into(PhotosFragment.mPhotoFromPicasa);
+
+                    //newASync.onPostExecute(Integer.toString(currentPhoto));
+                    currentPhoto++;
+                    if(currentPhoto > 3) currentPhoto = 0;
+                    mTimerHandler.postDelayed(this, mDelay);
+                }
+            }, mDelay);
+        }
+    }
 
 }
