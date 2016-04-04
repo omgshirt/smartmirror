c4b2f1ac584b00b539eba8fe4b01b51cc02ac4af
Navin Row <navin.row.43@my.csun.edu>
2016-04-03 17:18:48 -0700

    Re-added a ScrollView import that was deleted when I pulled from dev.

--- app/src/main/java/org/main/smartmirror/smartmirror/GmailFragment.java
+++ app/src/main/java/org/main/smartmirror/smartmirror/GmailFragment.java
@@ -17,6 +17,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import com.google.android.gms.common.ConnectionResult;
