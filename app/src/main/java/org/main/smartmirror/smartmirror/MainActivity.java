358f6f83f259863c29b45a65c33d3ecc47c77e44
Harout <haroutter@gmail.com>
2016-04-13 20:15:16 -0700

    change display colors to black text/white background

--- app/src/main/java/org/main/smartmirror/smartmirror/MainActivity.java
+++ app/src/main/java/org/main/smartmirror/smartmirror/MainActivity.java
@@ -285,8 +285,8 @@ public class MainActivity extends AppCompatActivity
         int uiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                 | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                 | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_FULLSCREEN;
-        //| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION    // commented out to keep nav buttons for testing
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;    // commented out to keep nav buttons for testing
         //| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY // req API 19
         //| View.SYSTEM_UI_FLAG_IMMERSIVE;      // req API 19
         decorView.setSystemUiVisibility(uiOptions);
